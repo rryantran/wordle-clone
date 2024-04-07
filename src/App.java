3a79cf99f9a7e94f773d93a0d9629efe2eca861f
ryantran <113406848+rryantran@users.noreply.github.com>
2024-04-06 21:23:25 -0500

    test fxml

--- src/App.java
+++ src/App.java
@@ -1,7 +1,8 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
-import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -11,12 +12,16 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        Group root = new Group();
-        Scene scene = new Scene(root, Color.FLORALWHITE);
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/App.fxml"));
+            Scene scene = new Scene(root, Color.FLORALWHITE);
 
-        stage.setScene(scene);
-        stage.setTitle("Test");
+            stage.setScene(scene);
+            stage.setTitle("Test");
 
-        stage.show();
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
